e2cfc2df7327ecbad5f21a37db01e4f965218d66
jinwood93 <jinwood93@users.noreply.github.com>
2021-04-27 09:51:09 +0900

    스케줄러, 캘린더 (미완성)

---
+++ honey-front/src/calendar/data.js
@@ -0,0 +1,9 @@
+export const appointments = [
+    {
+        title : 'text1',
+        startDate : new Date(2021,4,27),
+        endDate : new Date(2021,4,28),
+        id : 0,
+        loaction : 'Room 1',
+    }
+]

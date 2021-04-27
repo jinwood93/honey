8e303e4aff6c00bf2e90dd3d5a1d9ab89c3f64ca
jinwood93 <jinwood93@users.noreply.github.com>
2021-04-27 19:32:29 +0900

    캘린더, 스케줄러 (Date Navigator 추가, schedular 옵션 추가)

---
+++ honey-front/src/calendar/AxiosAPI.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+export const apiVaseURL = 'http://localhost:3000/';
+
+dispatch({ type: FETCHING_COIN_DATA })
+return axios.get(`${apiBaseURL}/`, 
+{ headers = 
+{ 'X-CMC_PRO_API_KEY': 'apicode', } 
+})
--- honey-front/src/index.js
+++ honey-front/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {render} from "react-dom";
+// import Calendar from './calendar/Calendar';
+import Data2 from "../src/calendar/data"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(<Data2 />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

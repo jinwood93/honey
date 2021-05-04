ca207392deb26c476d418deee2550247ebd84fc8
jinwood93 <jinwood93@users.noreply.github.com>
2021-05-04 11:02:07 +0900

    스케줄러 프론트 업로드

---
+++ honey-front/진우_front/client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import "react-datetime/css/react-datetime.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
